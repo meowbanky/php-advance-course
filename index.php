b93cfbd02761901841b702a3e9bdcbc749506a46
meow_banky@yahoo.com <you@example.com>
2023-12-30 18:07:00 +0000

    direct

--- index.php
+++ index.php
@@ -31,11 +31,16 @@ $apple->set_name("apple");
 $apple->set_color("black");
 
 $banana = new Fruit;
-
 $banana->set_name("banana");
+$banana->set_color('orange');
 
-echo $banana->get_name();
+// echo $banana->get_name();
 echo '<br>';
-echo $apple->get_color();
+echo $banana->name;
 echo '<br>';
-echo $apple->get_name();
+echo $banana->color;
+// echo $apple->get_color();
+echo '<br>';
+echo $apple->name;
+echo '<br>';
+echo $apple->color;
